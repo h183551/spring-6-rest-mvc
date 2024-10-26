749357217ba3284b42d276b385a6d5bc5bfaca74
Hassan M <hasany.mahmood@gmail.com>
2024-10-26 02:25:15 +0200

    End of lecture 53 by HM

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Class BeerServiceImpl
+ * <p>
+ * Description: [Add class description here]
+ * <p>
+ * Created by hasan on 10/26/2024.
+ */
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public Beer getBeerById(UUID id) {
+        return Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+    }
+}
