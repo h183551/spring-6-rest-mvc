2013c50266866ae78d6e29d55326f9fe54d15e6b
Hassan M <hasany.mahmood@gmail.com>
2024-11-07 01:42:40 +0100

    End of lecture 65 by HM

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -96,4 +96,16 @@ public class BeerServiceImpl implements BeerService {
         beerMap.put(savedBeer.getId(), savedBeer);
         return savedBeer;
     }
+
+    @Override
+    public void updateBeerById(UUID beerId, Beer beer) {
+        Beer existing = beerMap.get(beerId);
+        existing.setBeerName(beer.getBeerName());
+        existing.setPrice(beer.getPrice());
+        existing.setUpc(beer.getUpc());
+        existing.setQuantityOnHand(beer.getQuantityOnHand());
+        existing.setBeerStyle(beer.getBeerStyle());
+
+        beerMap.put(existing.getId(), existing);
+    }
 }
