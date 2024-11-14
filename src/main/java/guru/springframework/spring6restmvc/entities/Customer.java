541a6a70bfba90261deb226e81e4f3efbe435ee7
Hassan M <hasany.mahmood@gmail.com>
2024-11-14 02:21:57 +0100

    End of lecture 94 by HM

--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring6restmvc.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
 import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -24,6 +26,8 @@ import java.util.UUID;
 public class Customer {
 
     @Id
+    @UuidGenerator
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     private String customerName;
 
