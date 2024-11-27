e0846dceaca1b574cf8de85a4421a94c4ed786ab
Hassan M <hasany.mahmood@gmail.com>
2024-11-27 02:08:44 +0100

    End of lecture 116 by HM

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -1,25 +1,35 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.mappers.BeerMapper;
 import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -32,11 +42,35 @@ class BeerControllerIT {
     @Autowired
     BeerMapper beerMapper;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
-    void testPatchByIdNotFound() {
+    void testPatchBeerBadName() throws Exception {
+        Beer beer = beerRepository.findAll().getFirst();
+
+        Map<String, Object> beerMap = new HashMap<>();
+        beerMap.put("beerName", "New Name that is very long and used to test failure of acceding constraints of size");
+
+        mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerMap)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testPatchBeerIdNotFound() {
         assertThrows(NotFoundException.class, () -> {
             beerController.updateBeerPatchById(UUID.randomUUID(), BeerDTO.builder().build());
         });
@@ -45,7 +79,7 @@ class BeerControllerIT {
     @Rollback
     @Transactional
     @Test
-    void testPatchById() {
+    void testPatchBeer() {
         Beer beer = beerRepository.findAll().getFirst();
         final String newBeerName = "PATCHED NAME";
         BeerDTO beerDTO = BeerDTO.builder()
@@ -56,7 +90,7 @@ class BeerControllerIT {
         final int originalVersion = beer.getVersion();
         ResponseEntity responseEntity = beerController.updateBeerPatchById(beer.getId(), beerDTO);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
-        entityManager.flush();
+        beerRepository.flush();
         Beer updatedBeer = beerRepository.findById(beer.getId()).get();
         assertThat(updatedBeer.getBeerName()).isEqualTo(newBeerName);
         assertThat(updatedBeer.getUpc()).isEqualTo(originalBeerUpc);
@@ -100,6 +134,9 @@ class BeerControllerIT {
         final String beerName = "UPDATED Using PUT";
         BeerDTO beerDTO = BeerDTO.builder()
                 .beerName(beerName)
+                .beerStyle(BeerStyle.PALE_ALE)
+                .price(new BigDecimal("27.12"))
+                .upc("123123123213")
                 .build();
         //I removed the two lines below because the version is handled automatically I do not want to override it
         //and also the service do not need the setId because it is passed as parameter
@@ -110,10 +147,9 @@ class BeerControllerIT {
         final int originalVersion = beer.getVersion();
         ResponseEntity responseEntity = beerController.updateById(beer.getId(), beerDTO);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
-        entityManager.flush();
+        beerRepository.flush();
         Beer updatedBeer = beerRepository.findById(beer.getId()).get();
         assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
-        assertThat(updatedBeer.getBeerStyle()).isNull();
         assertThat(updatedBeer.getVersion()).isEqualTo(originalVersion+1);
     }
 
